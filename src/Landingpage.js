616f1b2421a987f61541cc7e27d1e9688297c2a8
Evilcmd <tejonweb28@gmail.com>
2024-08-19 13:08:07 +0530

    minor updates

--- src/Landingpage.js
+++ src/Landingpage.js
@@ -1,13 +1,8 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
 import AppAppBar from './components/AppAppBar';
 import Hero from './components/Hero';
 import LogoCollection from './components/LogoCollection';
@@ -19,51 +14,10 @@ import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import getLPTheme from './getLPTheme';
 
-function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                width: '100dvw',
-                position: 'fixed',
-                bottom: 24,
-            }}
-        >
-            <ToggleButtonGroup
-                color="primary"
-                exclusive
-                value={showCustomTheme}
-                onChange={toggleCustomTheme}
-                aria-label="Platform"
-                sx={{
-                    backgroundColor: 'background.default',
-                    '& .Mui-selected': {
-                        pointerEvents: 'none',
-                    },
-                }}
-            >
-                <ToggleButton value>
-                    <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
-                    Custom theme
-                </ToggleButton>
-                <ToggleButton value={false}>Material Design 2</ToggleButton>
-            </ToggleButtonGroup>
-        </Box>
-    );
-}
 
-ToggleCustomTheme.propTypes = {
-    showCustomTheme: PropTypes.shape({
-        valueOf: PropTypes.func.isRequired,
-    }).isRequired,
-    toggleCustomTheme: PropTypes.func.isRequired,
-};
 
 export default function LandingPage() {
-    const [mode, setMode] = React.useState('light');
-    const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+    const [mode, setMode] = React.useState('dark');
     const LPtheme = createTheme(getLPTheme(mode));
     const defaultTheme = createTheme({ palette: { mode } });
 
@@ -71,12 +25,8 @@ export default function LandingPage() {
         setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
     };
 
-    const toggleCustomTheme = () => {
-        setShowCustomTheme((prev) => !prev);
-    };
-
     return (
-        <ThemeProvider theme={showCustomTheme ? LPtheme : defaultTheme}>
+        <ThemeProvider theme={LPtheme}>
             <CssBaseline />
             <AppAppBar mode={mode} toggleColorMode={toggleColorMode} />
             <Hero />
@@ -94,10 +44,6 @@ export default function LandingPage() {
                 <Divider />
                 <Footer />
             </Box>
-            <ToggleCustomTheme
-                showCustomTheme={showCustomTheme}
-                toggleCustomTheme={toggleCustomTheme}
-            />
         </ThemeProvider>
     );
 }
