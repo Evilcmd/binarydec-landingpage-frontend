d6cfd002c5b7dbb3971e99b841e4d06d6642974d
Immanuel Dsouza <dsouzad681@gmail.com>
2024-08-20 18:03:28 +0530

    added rfined hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -106,8 +106,8 @@ export default function Hero() {
                         width: '100%',
                         backgroundImage:
                             theme.palette.mode === 'light'
-                                ? 'url("/static/code-pic-1.png")'
-                                : 'url("/static/code-pic-2.png")',
+                                ? 'url("/static/enhancedImg1.png")'
+                                : 'url("/static/enhancedImg2.png")',
                         backgroundSize: 'cover',
                         borderRadius: '10px',
                         outline: '3px solid',
