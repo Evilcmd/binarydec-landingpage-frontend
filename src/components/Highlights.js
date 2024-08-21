e0efd65e0a29eea371d4f0cb7d79b08c649884f2
Immanuel Dsouza <dsouzad681@gmail.com>
2024-08-21 20:42:16 +0530

    added images in hightlight section and added boxshadow to highlights section

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -11,19 +11,26 @@ import ThumbUpAltRoundedIcon from '@mui/icons-material/ThumbUpAltRounded';
 
 const items = [
     {
-        icon: <SettingsSuggestRoundedIcon />,
+        icon:<Box sx={{
+            width:"50px",
+            
+            
+            height:"50px",
+        }}><img width={60} height={60} style={{padding:"4px",}} src='/static/development.png'/></Box> ,
         title: 'Hire Production Ready Engineers Who Transform Ideas into Impactful Solutions',
         description:
             'Candidates tested on real-world scenarios are more likely to be ready to jump into ongoing projects, reducing the ramp-up time needed to get them productive.',
     },
     {
-        icon: <ConstructionRoundedIcon />,
+        icon:<Box><img src='/static/growth.png' width={50} height={50}/></Box>, 
+        // <ConstructionRoundedIcon />,
         title: 'Evaluate problem-solving skills of a candidate by immersing them in realistic, job-like scenarios that closely mimic the actual challenges they\'ll face in the role.',
         description:
             'By focusing on practical skills, you can gauge how efficiently a candidate can apply their knowledge to deliver results in a real-world environment.',
     },
     {
-        icon: <ThumbUpAltRoundedIcon />,
+        icon:<Box><img src='/static/requirements.png' width={50} height={50}/></Box>, 
+        // <ThumbUpAltRoundedIcon />,
         title: 'Assess their skills by seeing how they tackle and complete real-world projects, showcasing their practical expertise and problem-solving abilities.',
         description:
             'Real-world tests assess a candidate\'s ability to solve problems they\'ll actually encounter on the job, ensuring their skills align with day-to-day tasks.',
@@ -72,10 +79,11 @@ export default function Highlights() {
                                 sx={{
                                     p: 3,
                                     height: '100%',
-                                    border: '1px solid',
-                                    borderColor: 'grey.800',
+                                    border: '2px solid',
+                                    borderColor: 'grey.600',
                                     background: 'transparent',
-                                    backgroundColor: 'grey.900',
+                                    backgroundColor: 'grey.800',
+                                    boxShadow:'0px 4px 20px rgba(242, 38, 19, 1)',
                                 }}
                             >
                                 <Box sx={{ opacity: '50%' }}>{item.icon}</Box>
